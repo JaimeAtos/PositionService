d06347e62c2aba2fd2e953ec9bfe962246843b6c
David Hdez <david.2.hernandez@atos.net>
2023-05-03 18:24:34 -0600

    Fixed configuration for mass transit

--- src/Core/Application/DependencyContainer.cs
+++ src/Core/Application/DependencyContainer.cs
@@ -18,16 +18,14 @@ public static class DependencyContainer
 			cfg.UsingRabbitMq((ctx, cfgrmq) =>
 			{
 				cfgrmq.Host("amqp://guest:guest@localhost:5672");
-				cfgrmq.ReceiveEndpoint("PositionServiceQueue", econfigureEndpoint =>
-				{
-					econfigureEndpoint.ConfigureConsumeTopology = false;
-					econfigureEndpoint.Durable = true;
-					econfigureEndpoint.UseMessageRetry(retryConfigure =>
+				cfgrmq.ReceiveEndpoint("PositionServiceQueue", configureEndpoint =>
+				{ configureEndpoint.ConfigureConsumeTopology = false;
+					configureEndpoint.Durable = true;
+					configureEndpoint.UseMessageRetry(retryConfigure =>
 					{
 						retryConfigure.Interval(5, TimeSpan.FromMilliseconds(1000));
 					});
-					// econfigureEndpoint.ConfigureConsumer<PositionCreatedConsumer>(ctx);
-					econfigureEndpoint.Bind("Atos.Core.EventsDTO:PositionCreated", d =>
+					configureEndpoint.Bind("Atos.Core.EventsDTO:PositionCreated", d =>
 					{
 						d.ExchangeType = "topic";
 						d.RoutingKey = "position.created";
@@ -35,6 +33,41 @@ public static class DependencyContainer
 				});
 
 				cfgrmq.Publish<PositionCreated>(x =>
+				{
+					x.ExchangeType = "topic";
+				});
+				cfgrmq.Publish<PositionUpdated>(x =>
+				{
+					x.ExchangeType = "topic";
+				});
+				cfgrmq.Publish<PositionDeleted>(x =>
+				{
+					x.ExchangeType = "topic";
+				});
+				
+				
+				cfgrmq.Publish<PositionSkillCreated>(x =>
+				{
+					x.ExchangeType = "topic";
+				});
+				cfgrmq.Publish<PositionSkillUpdated>(x =>
+				{
+					x.ExchangeType = "topic";
+				});
+				cfgrmq.Publish<PositionSkillDeleted>(x =>
+				{
+					x.ExchangeType = "topic";
+				});
+				
+				cfgrmq.Publish<ResourcePositionCreated>(x =>
+				{
+					x.ExchangeType = "topic";
+				});
+				cfgrmq.Publish<ResourcePositionUpdated>(x =>
+				{
+					x.ExchangeType = "topic";
+				});
+				cfgrmq.Publish<ResourcePositionDeleted>(x =>
 				{
 					x.ExchangeType = "topic";
 				});
